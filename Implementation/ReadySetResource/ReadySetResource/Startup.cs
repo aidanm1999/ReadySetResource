c19b2485cae7cc5c01d3a00931a5d6892cef9aca
Aidan Marshall <36016925+aidanm1999@users.noreply.github.com>
2018-03-29 20:14:55 +0100

    Added the project!

---
+++ Implementation/ReadySetResource/ReadySetResource/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ReadySetResource.Startup))]
+namespace ReadySetResource
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
