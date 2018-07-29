52a485ed047b7cbca618a5743fe60b9f070693da
Aidan Marshall <36016925+aidanm1999@users.noreply.github.com>
2018-07-29 02:19:39 +0100

    Continued the frontend development

--- Implementation/ReadySetResource/ReadySetResource/Areas/Apps/Controllers/MyAccountController.cs
+++ Implementation/ReadySetResource/ReadySetResource/Areas/Apps/Controllers/MyAccountController.cs
@@ -21,7 +21,7 @@ using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace ReadySetResource.Areas.Apps.Controllers
 {
-    public class AccountController : Controller
+    public class MyAccountController : Controller
     {
 
         #region Initialization and StartUp
@@ -30,7 +30,7 @@ namespace ReadySetResource.Areas.Apps.Controllers
         private ApplicationUserManager _userManager;
 
 
-        public AccountController()
+        public MyAccountController()
         {
             _context = new ApplicationDbContext();
 
@@ -188,6 +188,112 @@ namespace ReadySetResource.Areas.Apps.Controllers
             }
         }
         #endregion
+
+        #endregion
+
+        #region Index
+
+        #region Get
+        [HttpGet]
+        [Authorize]
+        public ActionResult SelectAvitar()
+        {
+            var userId = User.Identity.GetUserId();
+            ApplicationUser user = _context.Users.SingleOrDefault(c => c.Id == userId);
+
+            return View(user);
+        }
+        #endregion
+
+        #region Set
+        [HttpPost]
+        [Authorize]
+        public ActionResult SelectAvitar(SettingsViewModel settingsVM)
+        {
+            //Finds the user and sets the user
+
+            ApplicationUser userInDb = _context.Users.SingleOrDefault(u => u.Id == settingsVM.User.Id);
+
+
+
+            //Checks to see if there are any changes to the shift 
+            //If not, error message is that no changes have been made
+            bool changesMade = false;
+
+
+            if (userInDb.AccessFailedCount != settingsVM.User.AccessFailedCount |
+                userInDb.AddressLine1 != settingsVM.User.AddressLine1 |
+                userInDb.AddressLine2 != settingsVM.User.AddressLine2 |
+                userInDb.BackupEmail != settingsVM.User.BackupEmail |
+                userInDb.Blocked != settingsVM.User.Blocked |
+                userInDb.BusinessUserTypeId != settingsVM.User.BusinessUserTypeId |
+                userInDb.DateOfBirth != settingsVM.TempBirthDate |
+                userInDb.Email != settingsVM.User.Email |
+                userInDb.EmailConfirmed != settingsVM.User.EmailConfirmed |
+                userInDb.EmergencyContact != settingsVM.User.EmergencyContact |
+                userInDb.EmployeeTypeId != settingsVM.User.EmployeeTypeId |
+                userInDb.FirstName != settingsVM.User.FirstName |
+                userInDb.LastName != settingsVM.User.LastName |
+                userInDb.NIN != settingsVM.User.NIN |
+                userInDb.PhoneNumber != settingsVM.User.PhoneNumber |
+                userInDb.PhoneNumberConfirmed != settingsVM.User.PhoneNumberConfirmed |
+                userInDb.Postcode != settingsVM.User.Postcode |
+                userInDb.Raise != settingsVM.User.Raise |
+                userInDb.Sex != settingsVM.User.Sex |
+                userInDb.Strikes != settingsVM.User.Strikes |
+                userInDb.Title != settingsVM.User.Title |
+                userInDb.UserName != settingsVM.User.UserName)
+            {
+                changesMade = true;
+
+
+                //userInDb.AccessFailedCount != settingsVM.User.AccessFailedCount;
+                //userInDb.AddressLine1 != settingsVM.User.AddressLine1;
+                //userInDb.AddressLine2 != settingsVM.User.AddressLine2;
+                //userInDb.BackupEmail != settingsVM.User.BackupEmail;
+                //userInDb.Blocked != settingsVM.User.Blocked;
+                //userInDb.BusinessUserTypeId != settingsVM.User.BusinessUserTypeId;
+                userInDb.DateOfBirth = settingsVM.TempBirthDate;
+                userInDb.Email = settingsVM.User.Email;
+                //userInDb.EmailConfirmed = settingsVM.User.EmailConfirmed;
+                //userInDb.EmergencyContact != settingsVM.User.EmergencyContact;
+                //userInDb.EmployeeTypeId != settingsVM.User.EmployeeTypeId;
+                userInDb.FirstName = settingsVM.User.FirstName;
+                userInDb.LastName = settingsVM.User.LastName;
+                //userInDb.NIN != settingsVM.User.NIN |
+                //userInDb.PhoneNumber != settingsVM.User.PhoneNumber |
+                //userInDb.PhoneNumberConfirmed != settingsVM.User.PhoneNumberConfirmed |
+                //userInDb.Postcode != settingsVM.User.Postcode |
+                //userInDb.Raise != settingsVM.User.Raise |
+                //userInDb.Sex != settingsVM.User.Sex |
+                //userInDb.Strikes != settingsVM.User.Strikes |
+                userInDb.Title = settingsVM.User.Title;
+                userInDb.UserName = settingsVM.User.UserName;
+
+                _context.SaveChanges();
+
+                return RedirectToAction("Index", "Dashboard");
+
+            }
+            else
+            {
+
+
+                if (changesMade == false)
+                {
+                    settingsVM.ErrorMessage = "No changes were made";
+                    return View("Settings", settingsVM);
+                }
+                else
+                {
+                    settingsVM.ErrorMessage = "An error occured.";
+                    return View("Settings", settingsVM);
+                }
+
+            }
+        }
+        #endregion
+
         #endregion
 
 
